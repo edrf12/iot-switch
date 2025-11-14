6f72d67a7b53547ffdadfc3a45d74a3aedbf29c6
edrf12 <me@edrf.party>
2025-11-13 22:49:17 -0300

    chore: Use pin numbers

--- src/pins.h
+++ src/pins.h
@@ -1,20 +1,20 @@
 #ifndef REMOTE_H_DEFS
 #define REMOTE_H_DEFS 1
 
-#define IRRX_PIN A0
-#define IRTX_PIN D3
+#define IRRX_PIN 0
+#define IRTX_PIN 21
 
-#define LIGHT_PIN A1
+#define LIGHT_PIN 1
 
 #define DHT_TYPE DHT22
-#define DHT_PIN A2
+#define DHT_PIN 2
 
 #define SCREEN_ADDRESS 0x3D
 #define SCREEN_HEIGHT 64
 #define SCREEN_WIDTH 128
 
-#define RELAY_PIN D9
+#define RELAY_PIN 20
 
-#define TOUCH_PIN D10
+#define TOUCH_PIN 18
 
 #endif  // REMOTE_H_DEFS
