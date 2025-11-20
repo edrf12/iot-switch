9cad0e9a1d7d73f22f956ff1988e17b32fd2ee11
edrf12 <me@edrf.party>
2025-11-20 20:50:43 -0300

    chore: Change pin defs

--- src/pins.h
+++ src/pins.h
@@ -9,12 +9,12 @@
 #define DHT_TYPE DHT22
 #define DHT_PIN 2
 
-#define SCREEN_ADDRESS 0x3D
+#define SCREEN_ADDRESS 0x3C
 #define SCREEN_HEIGHT 64
 #define SCREEN_WIDTH 128
 
 #define RELAY_PIN 20
 
-#define TOUCH_PIN 18
+#define TOUCH_PIN 19
 
 #endif  // REMOTE_H_DEFS
